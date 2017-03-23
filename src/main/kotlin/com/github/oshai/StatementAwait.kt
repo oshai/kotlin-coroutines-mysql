1e6076bfac81e82d67d4f3c1abc979ab7f484544
Ohad Shai <oshai@outbrain.com>
2017-03-23 21:35:17 +0200

    add initial implementation

--- src/main/kotlin/com/github/oshai/StatementAwait.kt
+++ src/main/kotlin/com/github/oshai/StatementAwait.kt
@@ -1,11 +1,23 @@
 package com.github.oshai
 
 import com.github.mauricio.async.db.Connection
-import scala.collection.JavaConverters
+import com.github.mauricio.async.db.QueryResult
+import kotlinx.coroutines.experimental.future.await
+import scala.collection.JavaConversions
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.CompletionStage
 
-class StatementAwait {
+object StatementAwait {
 
-    fun await(connection: Connection, statement: String, vararg values: Any) {
-        val future = connection.sendPreparedStatement(statement, JavaConverters.asScalaBuffer(values.toList()).toSeq())
+    fun QueryStatement.sendPreparedStatement(connection: Connection): CompletableFuture<QueryResult> {
+        val future = connection.sendPreparedStatement(this.statement, JavaConversions.asScalaBuffer(this.values).toSeq())
+        val javaFuture: CompletionStage<QueryResult> = scala.compat.java8.FutureConverters.toJava(future)
+        return (javaFuture as CompletableFuture<QueryResult>)
+    }
+
+    suspend fun CompletableFuture<QueryResult>.awaitQuery() : QueryResult {
+        return this.await()
     }
 }
+
+data class QueryStatement(val statement: String, val values: List<Any>)
